105371701a4eb03207d0fce17c3c8e17c1c9124e
Tobias Fajks <toby.fajks@gmail.com>
2026-01-01 23:09:04 +0100

    beggining of the project, created menu, ability to write into file without overwriting previous entry.

---
+++ journal.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+#define password "test123";
+using namespace std;
+
+fstream file("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal\\entries.txt", fstream::app);
+void menu() 
+{
+	cout << "                                   " << endl;
+	cout << "*____________ JOURNAL ____________*" << endl;
+	cout << "|                                 |" << endl;
+	cout << "|          [1]NEW ENTRY           |" << endl;
+	cout << "|          [2]READ ENTRY          |" << endl;
+}
+int main()
+{
+	file << "gay boy ja ja ja";
+	menu();
+}
